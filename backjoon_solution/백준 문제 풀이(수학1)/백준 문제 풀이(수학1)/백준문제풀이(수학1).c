a10cca400d426d89817fa095681beace828dff26
jong du <parksn5029@naver.com>
2020-07-22 20:40:14 +0900

    Study_Backjoon_math_01

--- backjoon_solution/백준 문제 풀이(수학1)/백준 문제 풀이(수학1)/백준문제풀이(수학1).c
+++ backjoon_solution/백준 문제 풀이(수학1)/백준 문제 풀이(수학1)/백준문제풀이(수학1).c
@@ -80,19 +80,67 @@ int main() {
 
 	// 분수찾기
 
-	int X, count=1;
-	int A=1, B=1;
-	scanf("%d", &X);
+	/*int X, key, count = 2;
+	int i = 2, j = 1, k = 2;
+
+
+	scanf("%d",&X);
 
 	if (1 <= X && X <= 10000000) {
 
-		if (A == 1 && B == 1) {
-			B++;
-			count++;
+		if (X == 1) {
+			printf("1/1");
+			return 0;
 		}
 
-		if()
+		while (1) {
+			if (i <= X && i + j >= X) {
+				if (j % 2 == 1) key = 0;
+				else key = 1;
+				break;
+			}
 
-	}
+			i = i + j + 1;
+			j++;
+		}
+
+		i = 2; j = 0; k = 0;
+
+		while (1) {
+			j = 1;
+			for (k = i; k > 0; k--) {
+				
+
+				if (X == count) {
+					printf("%d/%d", (key ==1) ? k:j, (key == 1) ? j:k  );
+					return 0;
+
+				}
+
+				j++;
+				count++;
+			}
+
+			i++;
+		}
+
+		
+	}*/
+
+	// 달팽이는 올라가고 싶다
+
+	int up, down, height;
+	int temp;
+	int day = 1;
+	scanf("%d %d %d", &up, &down, &height);
+
+	temp = (height - up) / (up - down);
+
+	if ((height - up) % (up - down) == 0)
+		printf("%d", day+temp);
+
+
+	else
+		printf("%d", day+temp+1);
 
 }
