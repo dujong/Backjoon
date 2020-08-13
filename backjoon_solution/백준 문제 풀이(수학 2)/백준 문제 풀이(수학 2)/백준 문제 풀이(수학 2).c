9d78ca11a1e54ad8fdf9fa4cd32624814fe1dfc0
jong du <parksn5029@naver.com>
2020-08-13 22:10:53 +0900

    Study_Backjoon_math_02

--- backjoon_solution/백준 문제 풀이(수학 2)/백준 문제 풀이(수학 2)/백준 문제 풀이(수학 2).c
+++ backjoon_solution/백준 문제 풀이(수학 2)/백준 문제 풀이(수학 2)/백준 문제 풀이(수학 2).c
@@ -4,7 +4,7 @@ void main() {
 
 	//소수 찾기
 
-	int N, number;
+	/*int N, number;
 	int count = 0;
 	int i,j;
 	scanf("%d", &N);
@@ -19,5 +19,31 @@ void main() {
 		if (number == j) count++;
 	}
 		printf("%d", count);
-		return 0;
+		return 0;*/
+		// 소수
+	int M, N, sum = 0, min = 0;
+	int i, j, arr[10000] = { 0, };
+
+	arr[0] = 1, arr[1] = 1;
+
+	for (i = 2; i < 10000/2; i++) 
+		for (j = i * i; j < 10000; j++) 
+			if (j % i == 0) arr[j] = 1;
+
+
+	scanf("%d %d", &M, &N);
+
+	for (i = M; i < N+1; i++) {
+
+		if (arr[i] == 0) {
+			sum += i;
+
+			if (min == 0) min = i;
+
+		}
+	}
+
+	(sum == 0) ? printf("-1") : printf("%d\n%d", sum, min);
+	return 0;
+
 }
