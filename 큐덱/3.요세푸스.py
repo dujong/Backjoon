8f978e0025d09d2285bb4a9fd1db44b3a2f1ee0e
jongdu <parksn5029@naver.com>
2021-12-10 14:00:25 +0900

    Study Baekjon 큐덱 12.10

---
+++ 큐덱/3.요세푸스.py
@@ -0,0 +1,14 @@
+count, num = map(int, input().split())
+ 
+josephus = [i for i in range(1, count+1)]
+ 
+result = []
+seqNo = num-1
+while len(josephus):
+    if seqNo >= len(josephus):
+        seqNo = seqNo % len(josephus)
+    else:
+        result.append(str(josephus.pop(seqNo)))
+        seqNo += (num-1)
+ 
+print("<", ", ".join(result), ">", sep='')
